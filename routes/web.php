ebb3b75934cb70d7ffbe8c49ac248890e8f0968c
André Glaser <andreglaser@fastmail.com>
2025-11-23 21:06:14 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/', function () {
-    \Log::info('Welcome page accessed');
+    \Log::info('Welcome page accessed', [
+        'user' => User::all(),
+    ]);
+    dd(User::all());
     return view('welcome');
 });
